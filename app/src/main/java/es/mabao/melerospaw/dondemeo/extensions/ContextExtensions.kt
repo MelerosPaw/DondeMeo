b09c7cda88362bb6cc5dc20f98c9854552d3b909
Juan José Melero <juanjose.melero@s-dos.es>
2018-07-25 01:02:19 +0200

    Added: starting to emulate selector.

--- app/src/main/java/es/mabao/melerospaw/dondemeo/extensions/ContextExtensions.kt
+++ app/src/main/java/es/mabao/melerospaw/dondemeo/extensions/ContextExtensions.kt
@@ -1,8 +1,19 @@
 package es.mabao.melerospaw.dondemeo.extensions
 
+import android.annotation.TargetApi
 import android.content.Context
+import android.os.Build
+import android.util.DisplayMetrics
 import android.widget.Toast
 
 fun Context.toast(message: String, duration: Int = Toast.LENGTH_SHORT) {
     Toast.makeText(this, message, duration).show()
 }
+
+fun kitKatApiLevel(executeIfKitKatOrHigher: () -> Unit) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+        executeIfKitKatOrHigher.invoke()
+    }
+}
+
+fun Context.toPixels(dpi: Float) = dpi * (resources.displayMetrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT)
--- app/src/main/java/es/mabao/melerospaw/dondemeo/ui/NewPeePointActivity.kt
+++ app/src/main/java/es/mabao/melerospaw/dondemeo/ui/NewPeePointActivity.kt
@@ -1,13 +1,17 @@
 package es.mabao.melerospaw.dondemeo.ui
 
+import android.annotation.TargetApi
 import android.app.Activity
+import android.os.Build
 import android.os.Bundle
+import android.transition.TransitionManager
 import android.view.MenuItem
-import android.view.MotionEvent
 import android.view.View
-import android.view.View.OnTouchListener
+import android.view.View.*
 import es.mabao.melerospaw.dondemeo.R
 import es.mabao.melerospaw.dondemeo.base.BaseActivity
+import es.mabao.melerospaw.dondemeo.extensions.kitKatApiLevel
+import es.mabao.melerospaw.dondemeo.extensions.toPixels
 import kotlinx.android.synthetic.main.activity_new_pee_point.*
 
 
@@ -43,10 +47,31 @@ class NewPeePointActivity : BaseActivity() {
             setResult(Activity.RESULT_OK)
             finish()
         }
+
+        new_pee_point__selected_soap.visibility = INVISIBLE
+        new_pee_point__label__there_is_soap.setOnClickListener(soapOnClickListener)
+        new_pee_point__label__there_isnt_soap.setOnClickListener(soapOnClickListener)
+        new_pee_point__label__havent_checked_soap.setOnClickListener(soapOnClickListener)
     }
 
     private fun goBackWithoutResult() {
         setResult(Activity.RESULT_CANCELED)
         finish()
+    }
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    private val soapOnClickListener = View.OnClickListener {
+        kitKatApiLevel { TransitionManager.beginDelayedTransition(new_pee_point__container__soap_selector) }
+        with(new_pee_point__selected_soap) {
+            x = it.x
+            y = it.y
+            val currentLayoutParams = layoutParams
+            currentLayoutParams.height = it.height
+            currentLayoutParams.width = it.width
+            layoutParams = currentLayoutParams
+            if (visibility != View.VISIBLE) {
+                visibility = VISIBLE
+            }
+        }
     }
 }
