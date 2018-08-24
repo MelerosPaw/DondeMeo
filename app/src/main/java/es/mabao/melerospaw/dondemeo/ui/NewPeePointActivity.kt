1438ac91f1cbc27a2b8eb5a24dcb56449f833425
Juan Jose Melero <meleros_paw@hotmail.com>
2018-08-25 00:44:22 +0200

    Added: intent to pick image from device.

--- app/src/main/java/es/mabao/melerospaw/dondemeo/ui/NewPeePointActivity.kt
+++ app/src/main/java/es/mabao/melerospaw/dondemeo/ui/NewPeePointActivity.kt
@@ -1,29 +1,20 @@
 package es.mabao.melerospaw.dondemeo.ui
 
-import android.animation.AnimatorSet
-import android.animation.ObjectAnimator
-import android.annotation.TargetApi
 import android.app.Activity
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import android.transition.TransitionManager
 import android.view.MenuItem
-import android.view.View
-import android.view.View.INVISIBLE
-import android.view.View.VISIBLE
-import android.widget.TextView
 import es.mabao.melerospaw.dondemeo.R
 import es.mabao.melerospaw.dondemeo.base.BaseActivity
-import es.mabao.melerospaw.dondemeo.extensions.findFirstChild
-import es.mabao.melerospaw.dondemeo.extensions.kitKatApiLevel
+import es.mabao.melerospaw.dondemeo.extensions.toast
 import kotlinx.android.synthetic.main.activity_new_pee_point.*
 
 
 class NewPeePointActivity : BaseActivity() {
 
     companion object {
-        val REQUEST_CODE: Int = 1
+        val REQUEST_CODE: Int = 0
+        val REQUEST_CODE_PICTURE: Int = 1
         val EXTRA_SOAP: String = "EXTRA_SOAP"
     }
 
@@ -44,65 +35,43 @@ class NewPeePointActivity : BaseActivity() {
         }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_CODE_PICTURE) {
+            toast("Tengo foto")
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
     override fun onBackPressed() {
         goBackWithoutResult()
     }
 
     private fun loadView() {
         new_pee_point__btn__done.setOnClickListener {
-            val data = Intent()
-            data.putExtra(EXTRA_SOAP, new_pee_point__selector__soap.getSelectedOption())
-            setResult(Activity.RESULT_OK, data)
-            finish()
+            goBackWithResult()
         }
+        new_pee_point__btn__add_image.setOnClickListener {
+            val intent = Intent()
+            intent.action = Intent.ACTION_PICK
+            intent.type = "image/*"
+            startActivityForResult(intent, REQUEST_CODE_PICTURE)
+        }
+    }
 
-        new_pee_point__selected_soap.visibility = INVISIBLE
-        new_pee_point__label__there_is_soap.setOnClickListener(soapOnClickListener)
-        new_pee_point__label__there_isnt_soap.setOnClickListener(soapOnClickListener)
-        new_pee_point__label__havent_checked_soap.setOnClickListener(soapOnClickListener)
+    fun goBackWithResult() {
+        val data = Intent()
+        collectResultData(data)
+        setResult(Activity.RESULT_OK, data)
+        finish()
+    }
+
+    fun collectResultData(data: Intent) {
+        data.putExtra(EXTRA_SOAP, new_pee_point__selector__soap.getSelectedOption())
     }
 
     private fun goBackWithoutResult() {
         setResult(Activity.RESULT_CANCELED)
         finish()
-    }
-
-    @TargetApi(Build.VERSION_CODES.KITKAT)
-    private val soapOnClickListener = View.OnClickListener {
-
-        with(new_pee_point__selected_soap) {
-
-            // Animates position
-            val animationX = ObjectAnimator.ofFloat(this, View.TRANSLATION_X, it.x)
-            val animationY = ObjectAnimator.ofFloat(this, View.TRANSLATION_Y, it.y)
-            val animatorSet = AnimatorSet()
-            animatorSet.playTogether(animationX, animationY)
-            animatorSet.start()
-
-            // Animates size
-            kitKatApiLevel { TransitionManager.beginDelayedTransition(new_pee_point__container__soap_selector) }
-            val currentLayoutParams = layoutParams
-            currentLayoutParams.height = it.height
-            currentLayoutParams.width = it.width
-            layoutParams = currentLayoutParams
-            if (visibility != View.VISIBLE) {
-                visibility = VISIBLE
-            }
-        }
-    }
-
-    private fun getSelectedSoapOption() : String? {
-        var selectedOption: String? = null
-
-        if (new_pee_point__selected_soap.visibility != VISIBLE) {
-            selectedOption = null
-        } else {
-            val selectedX = new_pee_point__selected_soap.x
-            val selectedView: TextView? = new_pee_point__container__soap_options.findFirstChild { it.x == selectedX }
-            if (selectedView != null) {
-                selectedOption = selectedView.text.toString()
-            }
-        }
-        return selectedOption
     }
 }
